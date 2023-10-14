4f2f5099c870a3928f03f825f73497b52064e657
tybeller <ty.c.beller@gmail.com>
2023-10-14 14:41:25 -0400

    added search

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY
 
 function App() {
   const [list, setList] = useState(null)
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
 
@@ -21,6 +23,21 @@ function App() {
 
   }, []);
 
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data))
+    }
+  }
+
   return (
    <>
     <div className="whole-page">
@@ -31,14 +48,24 @@ function App() {
         onChange={(inputString) => searchItems(inputString.target.value)}
       />
       <ul>
-        {list && Object.entries(list.Data).map(([coin]) =>
-          list.Data[coin].PlatformType === "blockchain" ? (
-            <CoinInfo
-              image={list.Data[coin].ImageUrl}
-              name={list.Data[coin].FullName}
-              symbol={list.Data[coin].Symbol}
-            />
-          ) : null
+        {searchInput.length > 0
+          ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )
+          : list && Object.entries(list.Data).map(([coin]) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
         )}
       </ul>
     </div>
